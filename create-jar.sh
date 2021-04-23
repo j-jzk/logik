d9ce03062a6c699a1efecbfb1b86c7951dbd8e8a
j-jzk <jjezek@protonmail.com>
2021-04-23 19:47:52 +0200

    use shadowJar for building a standalone JAR

--- create-jar.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-# Builds the project into one .jar instead of the ridiculous tarballs Gradle creates.
-# ("./gradlew jar" doesn't produce a runnable file)
-# If someone knows how to do this using Gradle, please tell me.
-
-kotlinc -include-runtime -d logik.jar src/main/kotlin/github/j_jzk/circuitsim/*
